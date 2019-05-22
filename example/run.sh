547c2f67e1531ac961aba43087d88a65d468dcb7
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2019-05-21 20:38:06 -0500

    fixede run.sh

--- example/run.sh
+++ example/run.sh
@@ -1,3 +1,4 @@
 docker-compose -f minio-compose.yml up -d 
 docker-compose -f s420-compose.yml up -d 
 echo "Now you can access to your files from web at 3300 and with gRPC at 24000"
+echo "run `go run upload.go` to upload an example image"
